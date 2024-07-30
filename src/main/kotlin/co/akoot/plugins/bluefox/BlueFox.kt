9c1f891f21bda2f320ceaa10b563fa117f3e3a0f
akoot <akoot@akoot.me>
2024-07-30 16:27:39 -0400

    Removed the so-called """compromising""" SQL stuff (not like it even mattered?)

--- src/main/kotlin/co/akoot/plugins/bluefox/BlueFox.kt
+++ src/main/kotlin/co/akoot/plugins/bluefox/BlueFox.kt
@@ -8,20 +8,13 @@ import java.sql.DriverManager
 
 class BlueFox : FoxPlugin() {
 
-    lateinit var connection: Connection
 
     override fun register() {
-        // Setup MySQL
-        Class.forName("com.mysql.cj.jdbc.Driver")
-        connection =
-            DriverManager.getConnection("jdbc:mysql://u12007_eur8XeXTIo:OE!qNLot%40wKAGZFnItKqQ9WT@mysql-1.us-mia.game.heavynode.net:3306/s12007_data") //TODO: Setup via config
-
         // Done
         logger.info("Good day!")
     }
 
     override fun unregister() {
         // Plugin shutdown logic
-        connection.close()
     }
 }
