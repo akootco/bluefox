b3ea7b9bd5c26bc837ccfb8a6acf5cd4c5c8b107
Akoot <akoot@akoot.me>
2025-11-24 17:49:53 -0500

    why didn't i do this 10 years ago

--- src/main/kotlin/co/akoot/plugins/bluefox/extensions/CommandSenderExtensions.kt
+++ src/main/kotlin/co/akoot/plugins/bluefox/extensions/CommandSenderExtensions.kt
@@ -1,6 +1,9 @@
 package co.akoot.plugins.bluefox.extensions
 
+import co.akoot.plugins.bluefox.util.Text
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
 val CommandSender.isBedrock: Boolean get() = this is Player && this.isBedrock
+
+fun CommandSender.sendMessage(text: Text) = sendMessage(text.component)
