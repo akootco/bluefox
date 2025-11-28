0d931256995e651c881a0c2937de124d6e6df7e4
Akoot <akoot@akoot.me>
2025-11-28 17:16:50 -0500

    TestCommand.kt shows a little of the new CatCommand format

--- src/main/kotlin/co/akoot/plugins/bluefox/commands/TestCommand.kt
+++ src/main/kotlin/co/akoot/plugins/bluefox/commands/TestCommand.kt
@@ -6,33 +6,41 @@ import com.mojang.brigadier.Command
 
 class TestCommand(plugin: FoxPlugin): CatCommand(plugin,"test") {
     init {
-        then(float("amount", -1f, 2f) {
+        then {
+            float("amount", -1f, 2f) {
                 val sender = getSender(it)
                 val amount = getFloat(it, "amount")
                 sender.sendMessage("very well $amount")
                 true
+            } then {
+                boolean {
+                    val sender = getSender(it)
+                    val amount = getFloat(it, "amount")
+                    val trueOrFalse = getBoolean(it)
+                    sender.sendMessage("now you did say $amount and $trueOrFalse")
+                    true
+                }
             }
-            .then(boolean() {
+        }
+        then {
+            subcommand("b") {
                 val sender = getSender(it)
-                val amount = getFloat(it, "amount")
-                val trueOrFalse = getBoolean(it)
-                sender.sendMessage("now you did say $amount and $trueOrFalse")
+                sender.sendMessage("you said b")
                 true
-            })
-        )
-        then(subcommand("b").then(player() {
-            val player = getPlayer(it)
-            getSender(it).sendMessage("am gonna krill ${player.name}")
-            true
-        }).executes {
-            val sender = getSender(it)
-            sender.sendMessage("you said b")
-            win
-        })
-        then(subcommand("c") {
-            val sender = getSender(it)
-            sender.sendMessage("you said c lol")
-            true
-        })
+            } then {
+                player {
+                    val player = getPlayer(it)
+                    getSender(it).sendMessage("am gonna krill ${player.name}")
+                    true
+                }
+            }
+        }
+        then {
+            subcommand("c") {
+                val sender = getSender(it)
+                sender.sendMessage("you said c lol")
+                true
+            }
+        }
     }
 }
